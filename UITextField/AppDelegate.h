59ff99b12ac62b2558e319b964e58eecb9450d6b
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-09 18:04:54 +0300

    Initial Commit

---
+++ UITextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITextField
+//
+//  Created by EnzoF on 09.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
